9ed755f2539ef5103396376eef82e7f6762a6662
a.vinogradov <a.vinogradov@bpcbt.com>
2021-12-06 22:44:05 +0300

    upd

--- install.sh
+++ install.sh
@@ -4,6 +4,24 @@ DIR=~/dotfiles
 
 wget -O ~/.zshrc        https://git.grml.org/f/grml-etc-core/etc/zsh/zshrc
 
+install_golang() {
+  local go_src target version latest_version
+
+  # purge old src
+  go_src="/usr/local/go"
+  if [[ -d "${go_src}" ]]; then
+    sudo rm -rf "${go_src}"
+  fi
+
+  latest_version="$(curl -sSL "https://golang.org/VERSION?m=text")"
+  set +u; version="${GO_VESION:-${latest_version}}"; set -u
+  target="https://dl.google.com/go/${version}.linux-amd64.tar.gz"
+
+  curl -sSL "${target}" | sudo tar -v -C /usr/local -xz
+}
+
+install_golang
+
 ## PREPARATIONS
 items=(
     'ssh'
