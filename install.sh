5a4eb6415cef9728916cfc85d0a0ba2dfd94cd7e
a.vinogradov <a.vinogradov@bpcbt.com>
2021-12-07 23:13:24 +0300

    upd

--- install.sh
+++ install.sh
@@ -5,6 +5,19 @@ DIR=~/dotfiles
 wget -O ~/.zshrc        https://git.grml.org/f/grml-etc-core/etc/zsh/zshrc
 
 ##
+## LN CONFIGS
+##
+items=(
+    'zshrc.local'
+    'zshrc.pre'
+    'gitconfig'
+)
+for item in $items; do
+  rm ~/.$item
+  ln -s $DIR/$item ~/.$item
+done
+
+##
 ## PREPARATIONS
 ##
 items=(
@@ -16,20 +29,9 @@ items=(
     'certs'
     'ru-locale'
     'install-go'
+    'git-extras'
+    'wsl'
 )
 for item in $items; do
   zsh $DIR/preparations/$item.sh
-done
-
-##
-## LN CONFIGS
-##
-items=(
-    'zshrc.local'
-    'zshrc.pre'
-    'gitconfig'
-)
-for item in $items; do
-  rm ~/.$item
-  ln -s $DIR/$item ~/.$item
 done
---
+++ preparations/zsh-git-extras.sh
@@ -0,0 +1,5 @@
+#!/bin/zsh
+
+DST=~/dotfiles/git-extras
+mkdir -p $DST
+wget -O $DST/git-extras-completion.zsh https://raw.githubusercontent.com/tj/git-extras/master/etc/git-extras-completion.zsh
