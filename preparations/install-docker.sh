3d16bb4c088768f4bff60fb582a7d5e242e2b636
Avin Lambrero <avin.github@gmail.com>
2024-05-05 12:02:02 +0300

    upd

--- preparations/install-docker.sh
+++ preparations/install-docker.sh
@@ -15,4 +15,6 @@ sudo apt-get update
 
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-sudo docker run hello-world
+sudo usermod -aG docker $USER
+
+docker run hello-world
