d1ee6a7640a83aef931d42b9f89df3d518cc825a
Avin Lambrero <avin.github@gmail.com>
2025-06-29 17:21:10 +0300

    upd

--- install_configs.sh
+++ install_configs.sh
@@ -12,6 +12,8 @@ rm -rf $HOME/.gitconfig
 rm -rf $HOME/.config/micro
 rm -rf $HOME/.config/tmux
 rm -rf $HOME/.config/starship.toml
+rm -rf $HOME/.config/powershell
+
 
 # Ensure config directory exists
 mkdir -p $HOME/.config
@@ -22,4 +24,5 @@ ln -sf "$SCRIPT_DIR/zshrc"         "$HOME/.zshrc"
 ln -sf "$SCRIPT_DIR/gitconfig"     "$HOME/.gitconfig"
 ln -sf "$SCRIPT_DIR/config/micro"  "$HOME/.config/micro"
 ln -sf "$SCRIPT_DIR/config/tmux"   "$HOME/.config/tmux"
+ln -sf "$SCRIPT_DIR/config/powershell"   "$HOME/.config/powershell"
 ln -sf "$SCRIPT_DIR/config/starship.toml" "$HOME/.config/starship.toml"
