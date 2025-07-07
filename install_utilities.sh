9912cc765e952b33ca8cede9c3bdbde43d7c80cc
Avin Lambrero <avin.github@gmail.com>
2025-07-07 18:12:44 +0300

    upd

--- install_utilities.sh
+++ install_utilities.sh
@@ -1,36 +1,30 @@
 #!/usr/bin/env zsh
-
 set -e
 
-# Determine script directory
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-# Prompt user to select components
 CHOICES=$(whiptail --title "Select components to install" --checklist \
-  "Choose components:" 15 60 6 \  
-  "install-rust"       "Rust" ON \
-  "install-go"         "Go" ON \
-  "install-go-utils"   "Go utilities" OFF \
-  "install-rust-utils" "Rust utilities" OFF \
-  "install-docker"     "Docker" OFF \
-  3>&1 1>&2 2>&3)
+"Choose components:" 15 60 8 \
+ssh                "Install SSH keys"     ON \
+install-micro      "Micro editor"         ON \
+install-rust       "Rust"                 ON \
+install-go         "Go"                   ON \
+install-go-utils   "Go utilities"         OFF \
+install-rust-utils "Rust utilities"      OFF \
+install-docker     "Docker"               OFF \
+3>&1 1>&2 2>&3)
 
-# Remove quotation marks
-CHOICES=$(echo $CHOICES | tr -d '"')
+# strip quotes from whiptail output
+CHOICES=${CHOICES//\"/}
 
-# Prepare list of preparation scripts to run
-items=('ssh' 'install-micro')
-for choice in $CHOICES; do
-  case $choice in
-    install-docker)     items+=('install-docker') ;;
-    install-rust)       items+=('install-rust') ;;
-    install-go)         items+=('install-go') ;;
-    install-go-utils)   items+=('install-go-utils') ;;
-    install-rust-utils) items+=('install-rust-utils') ;;
-  esac
-done
+# split into array on whitespace
+items=(${=CHOICES})
 
-# Execute selected preparation scripts
 for item in "${items[@]}"; do
-  zsh "$SCRIPT_DIR/preparations/$item.sh"
+  script="$SCRIPT_DIR/preparations/$item.sh"
+  if [ -f "$script" ]; then
+    zsh "$script"
+  else
+    echo "Warning: $script not found" >&2
+  fi
 done
--- preparations/ssh.sh
+++ preparations/ssh.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env zsh
-
 set -e
 
-echo "+++ Installing ssh keys +++"
+SSH_DIR="$HOME/.ssh"
+AUTH_KEYS="$SSH_DIR/authorized_keys"
+PUBLIC_KEY="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDMnUuUJsXw9j27uRL2x7szuQ/EmbmSxl6MHPwzsBrI1YWc6l0LBjNW0YR/VzsnEWMR5oAlSTsTnKGQ1DoqRJwfjadQSLf/jFl/CBoZ+7LSC8kIfENIvxIeOoZTQw921OGTwcvR8e2KLWrQpAWTFaBmeI3ntECBX8soaumIusS4chOL0NDhk8OhbAIpJk5oPm7dpe8O3CG9QfLXhkJHlY60YyG3ZR+vVWn/7YaUMn0KLn+yxQsYBRs+/FnfXlVvIgThRTCK0HRAIVP3aa7SEOjGvN2BVe1B3GLCp00+keBK99d5kSuQ+pcD+rk7NIw0G/jZgKAhT/2clr3XGr2+lLml avin@avin-pc"
 
-mkdir ~/.ssh -p
-echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDMnUuUJsXw9j27uRL2x7szuQ/EmbmSxl6MHPwzsBrI1YWc6l0LBjNW0YR/VzsnEWMR5oAlSTsTnKGQ1DoqRJwfjadQSLf/jFl/CBoZ+7LSC8kIfENIvxIeOoZTQw921OGTwcvR8e2KLWrQpAWTFaBmeI3ntECBX8soaumIusS4chOL0NDhk8OhbAIpJk5oPm7dpe8O3CG9QfLXhkJHlY60YyG3ZR+vVWn/7YaUMn0KLn+yxQsYBRs+/FnfXlVvIgThRTCK0HRAIVP3aa7SEOjGvN2BVe1B3GLCp00+keBK99d5kSuQ+pcD+rk7NIw0G/jZgKAhT/2clr3XGr2+lLml avin@avin-pc" >>~/.ssh/authorized_keys
-echo "$(uniq ~/.ssh/authorized_keys)" >~/.ssh/authorized_keys
-chmod 700 ~/.ssh
-chmod 600 ~/.ssh/authorized_keys
+mkdir -p "$SSH_DIR"
+chmod 700 "$SSH_DIR"
+
+# Add public key if it does not exist
+if ! grep -qxF "$PUBLIC_KEY" "$AUTH_KEYS" 2>/dev/null; then
+  echo "$PUBLIC_KEY" >> "$AUTH_KEYS"
+fi
+
+chmod 600 "$AUTH_KEYS"
