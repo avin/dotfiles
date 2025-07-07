a2d8cde530b5ee7a729f4a0786b2dd48ed629ff2
Avin Lambrero <avin.github@gmail.com>
2025-07-07 18:25:15 +0300

    upd

--- install_utilities.sh
+++ install_utilities.sh
@@ -7,8 +7,8 @@ CHOICES=$(whiptail --title "Select components to install" --checklist \
 "Choose components:" 15 60 8 \
 ssh                "Install SSH keys"     ON \
 install-micro      "Micro editor"         ON \
-install-rust       "Rust"                 ON \
-install-go         "Go"                   ON \
+install-rust       "Rust"                 OFF \
+install-go         "Go"                   OFF \
 install-go-utils   "Go utilities"         OFF \
 install-rust-utils "Rust utilities"      OFF \
 install-docker     "Docker"               OFF \
